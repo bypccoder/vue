c4359c9c3a1e4bae928512c27cc281b16560488e
bypccoder <carolina_pe_ca@hotmail.com>
2026-01-11 23:46:00 -0500

    Propiedades padre-hijo.

---
+++ src/composables/useWeather.js
@@ -0,0 +1,11 @@
+import { useWeatherStore } from "../stores/weatherStore";
+import { getTemperatura } from "../helpers/getWeather.js";
+
+export const useWeather = async () => {
+    const temperatura = await getTemperatura();
+    const weatherStore = useWeatherStore();
+    weatherStore.setTemperatura(temperatura);
+}
+
+
+
---
+++ src/helpers/getWeather.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const API_URL = 'https://api.open-meteo.com/v1/forecast?latitude=-12.1359&longitude=-77.0074&current=temperature_2m&timezone=auto&forecast_days=1';
+
+export const getTemperatura = async () => {
+    const respuesta = await axios.get(API_URL);
+    const nuevaTemperatura = respuesta.data.current.temperature_2m;
+
+    return nuevaTemperatura;
+}
